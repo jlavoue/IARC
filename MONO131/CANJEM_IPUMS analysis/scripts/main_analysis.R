4640a14e618c2de930a8d6a45f18a51282deb958
Jérôme Lavoué <jerome.lavoue@umontreal.ca>
2021-11-17 09:43:58 +0100

    V1.0 of IPUMS portrait by occupation

--- MONO131/CANJEM_IPUMS analysis/scripts/main_analysis.R
+++ MONO131/CANJEM_IPUMS analysis/scripts/main_analysis.R
@@ -80,62 +80,247 @@ opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
   
 #+ canjem cobalt , echo = FALSE 
 
-     canjem.pop.cobalt$isco.lab <- isco$Label[match( canjem.pop.cobalt$ISCO883D , isco$Value)]    
+     canjem.pop.cobalt.sum <- canjem.pop.cobalt   
+  
+     canjem.pop.cobalt.sum$isco.lab <- isco$Label[match( canjem.pop.cobalt.sum$ISCO883D , isco$Value)]    
  
-     canjem.pop.cobalt <- canjem.pop.cobalt[ order(canjem.pop.cobalt$p , decreasing = TRUE)[1:5] , c( 1 , 27 , 2 , 4 , 22 , 23 , 24) ] 
+     canjem.pop.cobalt.sum <- canjem.pop.cobalt.sum[ order(canjem.pop.cobalt.sum$p , decreasing = TRUE)[1:5] , c( 1 , 27 , 2 , 4 , 22 , 23 , 24) ] 
      
-     canjem.pop.cobalt$most.freq.confidence[ ] <- confidence$label[ match( canjem.pop.cobalt$most.freq.confidence , confidence$code)]
+     canjem.pop.cobalt.sum$most.freq.confidence[ ] <- confidence$label[ match( canjem.pop.cobalt.sum$most.freq.confidence , confidence$code)]
      
-     canjem.pop.cobalt$most.freq.intensity <- intensity$label[ match( canjem.pop.cobalt$most.freq.intensity , intensity$code )]
+     canjem.pop.cobalt.sum$most.freq.intensity <- intensity$label[ match( canjem.pop.cobalt.sum$most.freq.intensity , intensity$code )]
      
-     canjem.pop.cobalt$most.freq.frequency <- frequency$label[ match( canjem.pop.cobalt$most.freq.frequency , frequency$code)]
+     canjem.pop.cobalt.sum$most.freq.frequency <- frequency$label[ match( canjem.pop.cobalt.sum$most.freq.frequency , frequency$code)]
      
-     names( canjem.pop.cobalt ) <-c( "ISCO88" , "Title" , "n.jobs" , "p" , "confodence" , "intensity" , "frequency")
+     names( canjem.pop.cobalt.sum ) <-c( "ISCO88" , "Title" , "n.jobs" , "p" , "confidence" , "intensity" , "frequency")
      
-     canjem.pop.cobalt$p <- signif( canjem.pop.cobalt$p , 2 ) 
+     canjem.pop.cobalt.sum$p <- signif( canjem.pop.cobalt.sum$p , 2 ) 
      
-     knitr::kable( canjem.pop.cobalt , row.names = FALSE)
+     knitr::kable( canjem.pop.cobalt.sum , row.names = FALSE)
      
 #+ canjem antimony , echo = FALSE 
+
+     canjem.pop.antimony.sum <- canjem.pop.antimony
+          
+     canjem.pop.antimony.sum$isco.lab <- isco$Label[match( canjem.pop.antimony.sum$ISCO883D , isco$Value)]    
      
-     canjem.pop.antimony$isco.lab <- isco$Label[match( canjem.pop.antimony$ISCO883D , isco$Value)]    
+     canjem.pop.antimony.sum <- canjem.pop.antimony.sum[ order(canjem.pop.antimony.sum$p , decreasing = TRUE)[1:5] , c( 1 , 27 , 2 , 4 , 22 , 23 , 24) ] 
      
-     canjem.pop.antimony <- canjem.pop.antimony[ order(canjem.pop.antimony$p , decreasing = TRUE)[1:5] , c( 1 , 27 , 2 , 4 , 22 , 23 , 24) ] 
+     canjem.pop.antimony.sum$most.freq.confidence[ ] <- confidence$label[ match( canjem.pop.antimony.sum$most.freq.confidence , confidence$code)]
      
-     canjem.pop.antimony$most.freq.confidence[ ] <- confidence$label[ match( canjem.pop.antimony$most.freq.confidence , confidence$code)]
+     canjem.pop.antimony.sum$most.freq.intensity <- intensity$label[ match( canjem.pop.antimony.sum$most.freq.intensity , intensity$code )]
      
-     canjem.pop.antimony$most.freq.intensity <- intensity$label[ match( canjem.pop.antimony$most.freq.intensity , intensity$code )]
+     canjem.pop.antimony.sum$most.freq.frequency <- frequency$label[ match( canjem.pop.antimony.sum$most.freq.frequency , frequency$code)]
      
-     canjem.pop.antimony$most.freq.frequency <- frequency$label[ match( canjem.pop.antimony$most.freq.frequency , frequency$code)]
+     names( canjem.pop.antimony.sum ) <-c( "ISCO88" , "Title" , "n.jobs" , "p" , "confidence" , "intensity" , "frequency")
      
-     names( canjem.pop.antimony ) <-c( "ISCO88" , "Title" , "n.jobs" , "p" , "confodence" , "intensity" , "frequency")
+     canjem.pop.antimony.sum$p <- signif( canjem.pop.antimony.sum$p , 2 ) 
      
-     canjem.pop.antimony$p <- signif( canjem.pop.antimony$p , 2 ) 
-     
-     knitr::kable( canjem.pop.antimony , row.names = FALSE)     
+     knitr::kable( canjem.pop.antimony.sum , row.names = FALSE)     
      
 #+ canjem tungsten , echo = FALSE 
      
-     canjem.pop.tungsten$isco.lab <- isco$Label[match( canjem.pop.tungsten$ISCO883D , isco$Value)]    
+     canjem.pop.tungsten.sum <- canjem.pop.tungsten
      
-     canjem.pop.tungsten <- canjem.pop.tungsten[ order(canjem.pop.tungsten$p , decreasing = TRUE)[1:5] , c( 1 , 27 , 2 , 4 , 22 , 23 , 24) ] 
+     canjem.pop.tungsten.sum$isco.lab <- isco$Label[match( canjem.pop.tungsten.sum$ISCO883D , isco$Value)]    
      
-     canjem.pop.tungsten$most.freq.confidence[ ] <- confidence$label[ match( canjem.pop.tungsten$most.freq.confidence , confidence$code)]
+     canjem.pop.tungsten.sum <- canjem.pop.tungsten.sum[ order(canjem.pop.tungsten.sum$p , decreasing = TRUE)[1:5] , c( 1 , 27 , 2 , 4 , 22 , 23 , 24) ] 
      
-     canjem.pop.tungsten$most.freq.intensity <- intensity$label[ match( canjem.pop.tungsten$most.freq.intensity , intensity$code )]
+     canjem.pop.tungsten.sum$most.freq.confidence[ ] <- confidence$label[ match( canjem.pop.tungsten.sum$most.freq.confidence , confidence$code)]
      
-     canjem.pop.tungsten$most.freq.frequency <- frequency$label[ match( canjem.pop.tungsten$most.freq.frequency , frequency$code)]
+     canjem.pop.tungsten.sum$most.freq.intensity <- intensity$label[ match( canjem.pop.tungsten.sum$most.freq.intensity , intensity$code )]
      
-     names( canjem.pop.tungsten ) <-c( "ISCO88" , "Title" , "n.jobs" , "p" , "confodence" , "intensity" , "frequency")
+     canjem.pop.tungsten.sum$most.freq.frequency <- frequency$label[ match( canjem.pop.tungsten.sum$most.freq.frequency , frequency$code)]
      
-     canjem.pop.tungsten$p <- signif( canjem.pop.tungsten$p , 2 ) 
+     names( canjem.pop.tungsten.sum ) <-c( "ISCO88" , "Title" , "n.jobs" , "p" , "confidence" , "intensity" , "frequency")
      
-     knitr::kable( canjem.pop.tungsten , row.names = FALSE)  
+     canjem.pop.tungsten.sum$p <- signif( canjem.pop.tungsten.sum$p , 2 ) 
+     
+     knitr::kable( canjem.pop.tungsten.sum , row.names = FALSE)  
      
      
      
-#' **IPUMS analysis : portrait by country**
+#' **IPUMS analysis : portrait by occupation**
 #'
+#' *Cobalt*
+#' 
+
+#+ occupation cobalt, echo = FALSE
+
+     myisco <- 722
+     
+     ## making a table summarizing exposure info ( top 5 countries)
+     
+     occup.tab.cobalt <- data.frame( country = unique( ipums$country[ ipums$isco88a == myisco]  ) , stringsAsFactors = FALSE )
+     
+     occup.tab.cobalt$country.lab <- country$Label[ match( occup.tab.cobalt$country , country$Value)]
+     
+     occup.tab.cobalt$n.people <- signif( ipums$n_people[ ipums$isco88a == myisco][ match( occup.tab.cobalt$country , ipums$country[ ipums$isco88a == myisco]  )  ] , 2)
+     
+     occup.tab.cobalt$perc.people <- signif( 100*occup.tab.cobalt$n.people/ (1000000*mycountry$n.M[ match( occup.tab.cobalt$country , mycountry$country )] ) , 2)
+     
+     occup.tab.cobalt$estatus <- canjem.pop.cobalt$exposed[ canjem.pop.cobalt$ISCO883D == myisco ]
+     
+     occup.tab.cobalt$n.unexp <- signif( occup.tab.cobalt$n.people*canjem.pop.cobalt$p.C0[ canjem.pop.cobalt$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.cobalt$n.low <- signif( occup.tab.cobalt$n.people*canjem.pop.cobalt$p.C1[ canjem.pop.cobalt$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.cobalt$n.medium <- signif( occup.tab.cobalt$n.people*canjem.pop.cobalt$p.C2[ canjem.pop.cobalt$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.cobalt$n.high <- signif( occup.tab.cobalt$n.people*canjem.pop.cobalt$p.C3[ canjem.pop.cobalt$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.cobalt$most.freq.confidence <- ifelse( occup.tab.cobalt$estatus == "pot.exposed", 
+                                                      confidence$label[ confidence$code == canjem.pop.cobalt$most.freq.confidence[ canjem.pop.cobalt$ISCO883D == myisco ] ],
+                                                      NA)
+     
+     occup.tab.cobalt$most.freq.frequency <- ifelse( occup.tab.cobalt$estatus == "pot.exposed", 
+                                                     frequency$label[ frequency$code == canjem.pop.cobalt$most.freq.frequency[ canjem.pop.cobalt$ISCO883D == myisco ] ],
+                                                     NA)
+     
+     ## showing top 5 high exposed
+     
+     knitr::kable( occup.tab.cobalt[ order(occup.tab.cobalt$n.high , decreasing = TRUE)[1:5] , c(2:11) ] , row.names = FALSE) 
+     
+     ## global portrait
+     
+     occup.overall.cobalt <- data.frame( metric = c("total.n" , "total.perc" , "estatus" , "n.unexp" , "n.low" , "n.medium" , "n.high") , stringsAsFactors = FALSE )
+     
+     occup.overall.cobalt$value[1] <- sum(occup.tab.cobalt$n.people)
+     
+     occup.overall.cobalt$value[2] <- signif( 100*occup.overall.cobalt$value[1]/sum( mycountry$n.M * 1000000 ) , 2)
+       
+     occup.overall.cobalt$value[3] <- occup.tab.cobalt$estatus[1]
+       
+     occup.overall.cobalt$value[4] <- signif( sum( occup.tab.cobalt$n.unexp) , 2)
+       
+     occup.overall.cobalt$value[5] <- signif( sum( occup.tab.cobalt$n.low ) , 2)
+       
+     occup.overall.cobalt$value[6] <- signif( sum( occup.tab.cobalt$n.medium ) , 2)
+       
+     occup.overall.cobalt$value[7] <- signif( sum( occup.tab.cobalt$n.high ) , 2)
+     
+     knitr::kable( occup.overall.cobalt , row.names = FALSE) 
+     
+     
+#' *Antimony*
+#' 
+     
+#+ occupation antimony, echo = FALSE
+     
+     myisco <- 722
+     
+     ## making a table summarizing exposure info ( top 5 countries)
+     
+     occup.tab.antimony <- data.frame( country = unique( ipums$country[ ipums$isco88a == myisco]  ) , stringsAsFactors = FALSE )
+     
+     occup.tab.antimony$country.lab <- country$Label[ match( occup.tab.antimony$country , country$Value)]
+     
+     occup.tab.antimony$n.people <- signif( ipums$n_people[ ipums$isco88a == myisco][ match( occup.tab.antimony$country , ipums$country[ ipums$isco88a == myisco]  )  ] , 2)
+     
+     occup.tab.antimony$perc.people <- signif( 100*occup.tab.antimony$n.people/ (1000000*mycountry$n.M[ match( occup.tab.antimony$country , mycountry$country )] ) , 2)
+     
+     occup.tab.antimony$estatus <- canjem.pop.antimony$exposed[ canjem.pop.antimony$ISCO883D == myisco ]
+     
+     occup.tab.antimony$n.unexp <- signif( occup.tab.antimony$n.people*canjem.pop.antimony$p.C0[ canjem.pop.antimony$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.antimony$n.low <- signif( occup.tab.antimony$n.people*canjem.pop.antimony$p.C1[ canjem.pop.antimony$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.antimony$n.medium <- signif( occup.tab.antimony$n.people*canjem.pop.antimony$p.C2[ canjem.pop.antimony$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.antimony$n.high <- signif( occup.tab.antimony$n.people*canjem.pop.antimony$p.C3[ canjem.pop.antimony$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.antimony$most.freq.confidence <- ifelse( occup.tab.antimony$estatus == "pot.exposed", 
+                                                        confidence$label[ confidence$code == canjem.pop.antimony$most.freq.confidence[ canjem.pop.antimony$ISCO883D == myisco ] ],
+                                                        NA)
+     
+     occup.tab.antimony$most.freq.frequency <- ifelse( occup.tab.antimony$estatus == "pot.exposed",
+                                                       frequency$label[ frequency$code == canjem.pop.antimony$most.freq.frequency[ canjem.pop.antimony$ISCO883D == myisco ] ],
+                                                       NA)
+     
+     ## showing top 5 high exposed
+     
+     knitr::kable( occup.tab.antimony[ order(occup.tab.antimony$n.high , decreasing = TRUE)[1:5] , c(2:11) ] , row.names = FALSE) 
+     
+     ## global portrait
+     
+     occup.overall.antimony <- data.frame( metric = c("total.n" , "total.perc" , "estatus" , "n.unexp" , "n.low" , "n.medium" , "n.high") , stringsAsFactors = FALSE )
+     
+     occup.overall.antimony$value[1] <- sum(occup.tab.antimony$n.people)
+     
+     occup.overall.antimony$value[2] <- signif( 100*occup.overall.antimony$value[1]/sum( mycountry$n.M * 1000000 ) , 2)
+     
+     occup.overall.antimony$value[3] <- occup.tab.antimony$estatus[1]
+     
+     occup.overall.antimony$value[4] <- signif( sum( occup.tab.antimony$n.unexp) , 2)
+     
+     occup.overall.antimony$value[5] <- signif( sum( occup.tab.antimony$n.low ) , 2)
+     
+     occup.overall.antimony$value[6] <- signif( sum( occup.tab.antimony$n.medium ) , 2)
+     
+     occup.overall.antimony$value[7] <- signif( sum( occup.tab.antimony$n.high ) , 2)
+     
+     knitr::kable( occup.overall.antimony , row.names = FALSE) 
+     
 
      
+#' *Tungsten*
+#' 
      
+#+ occupation tungsten, echo = FALSE
+     
+     myisco <- 722
+     
+     ## making a table summarizing exposure info ( top 5 countries)
+     
+     occup.tab.tungsten <- data.frame( country = unique( ipums$country[ ipums$isco88a == myisco]  ) , stringsAsFactors = FALSE )
+     
+     occup.tab.tungsten$country.lab <- country$Label[ match( occup.tab.tungsten$country , country$Value)]
+     
+     occup.tab.tungsten$n.people <- signif( ipums$n_people[ ipums$isco88a == myisco][ match( occup.tab.tungsten$country , ipums$country[ ipums$isco88a == myisco]  )  ] , 2)
+     
+     occup.tab.tungsten$perc.people <- signif( 100*occup.tab.tungsten$n.people/ (1000000*mycountry$n.M[ match( occup.tab.tungsten$country , mycountry$country )] ) , 2)
+     
+     occup.tab.tungsten$estatus <- canjem.pop.tungsten$exposed[ canjem.pop.tungsten$ISCO883D == myisco ]
+     
+     occup.tab.tungsten$n.unexp <- signif( occup.tab.tungsten$n.people*canjem.pop.tungsten$p.C0[ canjem.pop.tungsten$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.tungsten$n.low <- signif( occup.tab.tungsten$n.people*canjem.pop.tungsten$p.C1[ canjem.pop.tungsten$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.tungsten$n.medium <- signif( occup.tab.tungsten$n.people*canjem.pop.tungsten$p.C2[ canjem.pop.tungsten$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.tungsten$n.high <- signif( occup.tab.tungsten$n.people*canjem.pop.tungsten$p.C3[ canjem.pop.tungsten$ISCO883D == myisco ]/100 , 2 )
+     
+     occup.tab.tungsten$most.freq.confidence <- ifelse( occup.tab.tungsten$estatus == "pot.exposed", 
+                                                        confidence$label[ confidence$code == canjem.pop.tungsten$most.freq.confidence[ canjem.pop.tungsten$ISCO883D == myisco ] ],
+                                                        NA)
+     
+     occup.tab.tungsten$most.freq.frequency <- ifelse( occup.tab.tungsten$estatus == "pot.exposed", 
+                                                       frequency$label[ frequency$code == canjem.pop.tungsten$most.freq.frequency[ canjem.pop.tungsten$ISCO883D == myisco ] ],
+                                                       NA)
+     
+     ## showing top 5 high exposed
+     
+     knitr::kable( occup.tab.tungsten[ order(occup.tab.tungsten$n.high , decreasing = TRUE)[1:5] , c(2:11) ] , row.names = FALSE) 
+     
+     ## global portrait
+     
+     occup.overall.tungsten <- data.frame( metric = c("total.n" , "total.perc" , "estatus" , "n.unexp" , "n.low" , "n.medium" , "n.high") , stringsAsFactors = FALSE )
+     
+     occup.overall.tungsten$value[1] <- sum(occup.tab.tungsten$n.people)
+     
+     occup.overall.tungsten$value[2] <- signif( 100*occup.overall.tungsten$value[1]/sum( mycountry$n.M * 1000000 ) , 2)
+     
+     occup.overall.tungsten$value[3] <- occup.tab.tungsten$estatus[1]
+     
+     occup.overall.tungsten$value[4] <- signif( sum( occup.tab.tungsten$n.unexp) , 2)
+     
+     occup.overall.tungsten$value[5] <- signif( sum( occup.tab.tungsten$n.low ) , 2)
+     
+     occup.overall.tungsten$value[6] <- signif( sum( occup.tab.tungsten$n.medium ) , 2)
+     
+     occup.overall.tungsten$value[7] <- signif( sum( occup.tab.tungsten$n.high ) , 2)
+     
+     knitr::kable( occup.overall.tungsten , row.names = FALSE) 
+     
+     
+          
--- MONO131/CANJEM_IPUMS analysis/scripts/main_analysis.md
+++ MONO131/CANJEM_IPUMS analysis/scripts/main_analysis.md
@@ -66,7 +66,7 @@ population by country
 
 *COBALT*
 
-| ISCO88 | Title                                               | n.jobs |    p | confodence | intensity | frequency |
+| ISCO88 | Title                                               | n.jobs |    p | confidence | intensity | frequency |
 |:-------|:----------------------------------------------------|-------:|-----:|:-----------|:----------|:----------|
 | 722    | Blacksmiths, tool-makers and related trades workers |    309 | 19.0 | probable   | high      | 2-12h     |
 | 731    | Precision workers in metal and related materials    |    115 | 12.0 | probable   | medium    | \<2h      |
@@ -74,7 +74,7 @@ population by country
 | 821    | Metal- and mineral-products machine operators       |    392 |  7.9 | probable   | medium    | 2-12h     |
 | 815    | Chemical-processing-plant operators                 |    115 |  6.1 | probable   | low       | 2-12h     |
 
-| ISCO88 | Title                                                    | n.jobs |    p | confodence | intensity | frequency |
+| ISCO88 | Title                                                    | n.jobs |    p | confidence | intensity | frequency |
 |:-------|:---------------------------------------------------------|-------:|-----:|:-----------|:----------|:----------|
 | 816    | Power-production and related plant operators             |    151 | 31.0 | possible   | low       | 2-12h     |
 | 734    | Printing and related trades workers                      |    133 | 25.0 | definite   | medium    | 40h+      |
@@ -82,10 +82,72 @@ population by country
 | 812    | Metal-processing-plant operators                         |    158 |  6.3 | probable   | low       | 40h+      |
 | 815    | Chemical-processing-plant operators                      |    115 |  2.6 | possible   | low       | 2-12h     |
 
-| ISCO88 | Title                                                     | n.jobs |     p | confodence | intensity | frequency |
+| ISCO88 | Title                                                     | n.jobs |     p | confidence | intensity | frequency |
 |:-------|:----------------------------------------------------------|-------:|------:|:-----------|:----------|:----------|
 | 722    | Blacksmiths, tool-makers and related trades workers       |    309 | 24.00 | probable   | high      | 2-12h     |
 | 821    | Metal- and mineral-products machine operators             |    392 |  8.40 | probable   | medium    | 2-12h     |
 | 312    | Computer associate professionals                          |     57 |  1.80 | definite   | low       | 2-12h     |
 | 315    | Safety and quality inspectors                             |    203 |  0.49 | possible   | low       | 40h+      |
 | 724    | Electrical and electronic equipment mechanics and fitters |    240 |  0.42 | possible   | low       | 2-12h     |
+
+**IPUMS analysis : portrait by occupation**
+
+*Cobalt*
+
+| country.lab    | n.people | perc.people | estatus     | n.unexp | n.low | n.medium | n.high | most.freq.confidence | most.freq.frequency |
+|:---------------|---------:|------------:|:------------|--------:|------:|---------:|-------:|:---------------------|:--------------------|
+| Iran           |   300000 |        0.49 | pot.exposed |  240000 | 18000 |     7800 |  31000 | probable             | 2-12h               |
+| United Kingdom |   230000 |        0.42 | pot.exposed |  190000 | 14000 |     6000 |  24000 | probable             | 2-12h               |
+| France         |   200000 |        0.34 | pot.exposed |  160000 | 12000 |     5200 |  21000 | probable             | 2-12h               |
+| Egypt          |   130000 |        0.18 | pot.exposed |  110000 |  8000 |     3400 |  13000 | probable             | 2-12h               |
+| Romania        |    96000 |        0.48 | pot.exposed |   78000 |  5900 |     2500 |   9900 | probable             | 2-12h               |
+
+| metric     | value       |
+|:-----------|:------------|
+| total.n    | 1516250     |
+| total.perc | 0.17        |
+| estatus    | pot.exposed |
+| n.unexp    | 1200000     |
+| n.low      | 92000       |
+| n.medium   | 39000       |
+| n.high     | 160000      |
+
+*Antimony*
+
+| country.lab | n.people | perc.people | estatus   | n.unexp | n.low | n.medium | n.high | most.freq.confidence | most.freq.frequency |
+|:------------|---------:|------------:|:----------|--------:|------:|---------:|-------:|:---------------------|:--------------------|
+| Armenia     |     2000 |       0.066 | unexposed |    2000 |     0 |        0 |      0 | NA                   | NA                  |
+| Bolivia     |    11000 |       0.130 | unexposed |   11000 |     0 |        0 |      0 | NA                   | NA                  |
+| Belarus     |    55000 |       0.580 | unexposed |   55000 |     0 |        0 |      0 | NA                   | NA                  |
+| Switzerland |    24000 |       0.330 | unexposed |   24000 |     0 |        0 |      0 | NA                   | NA                  |
+| Chile       |    17000 |       0.130 | unexposed |   17000 |     0 |        0 |      0 | NA                   | NA                  |
+
+| metric     | value     |
+|:-----------|:----------|
+| total.n    | 1516250   |
+| total.perc | 0.17      |
+| estatus    | unexposed |
+| n.unexp    | 1500000   |
+| n.low      | 0         |
+| n.medium   | 0         |
+| n.high     | 0         |
+
+*Tungsten*
+
+| country.lab    | n.people | perc.people | estatus     | n.unexp | n.low | n.medium | n.high | most.freq.confidence | most.freq.frequency |
+|:---------------|---------:|------------:|:------------|--------:|------:|---------:|-------:|:---------------------|:--------------------|
+| Iran           |   300000 |        0.49 | pot.exposed |  230000 | 23000 |    22000 |  27000 | probable             | 2-12h               |
+| United Kingdom |   230000 |        0.42 | pot.exposed |  170000 | 18000 |    17000 |  21000 | probable             | 2-12h               |
+| France         |   200000 |        0.34 | pot.exposed |  150000 | 16000 |    15000 |  18000 | probable             | 2-12h               |
+| Egypt          |   130000 |        0.18 | pot.exposed |   98000 | 10000 |     9700 |  12000 | probable             | 2-12h               |
+| Romania        |    96000 |        0.48 | pot.exposed |   73000 |  7500 |     7100 |   8700 | probable             | 2-12h               |
+
+| metric     | value       |
+|:-----------|:------------|
+| total.n    | 1516250     |
+| total.perc | 0.17        |
+| estatus    | pot.exposed |
+| n.unexp    | 1100000     |
+| n.low      | 120000      |
+| n.medium   | 110000      |
+| n.high     | 140000      |
